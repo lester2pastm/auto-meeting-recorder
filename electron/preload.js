5ebc0320eff049e341c48ee6a87dd34c1b713fe6
lester2pastm <25032501+lester2pastm@users.noreply.github.com>
2026-02-10 13:54:59 +0800

    update

--- electron/preload.js
+++ electron/preload.js
@@ -6,7 +6,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveAudio: (blob, filename) => ipcRenderer.invoke('save-audio', { blob, filename }),
   getAudio: (filename) => ipcRenderer.invoke('get-audio', filename),
   deleteAudio: (filename) => ipcRenderer.invoke('delete-audio', filename),
-  listAudioFiles: () => ipcRenderer.invoke('list-audio-files'),
   exportAudio: (filename, defaultPath) => ipcRenderer.invoke('export-audio', { filename, defaultPath }),
   getAudioDirectory: () => ipcRenderer.invoke('get-audio-directory'),
 
@@ -23,7 +22,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   // FFmpeg 相关接口（Linux 系统音频录制）
   checkFFmpeg: () => ipcRenderer.invoke('check-ffmpeg'),
   startFFmpegSystemAudio: (outputPath, device) => ipcRenderer.invoke('start-ffmpeg-system-audio', { outputPath, device }),
-  startFFmpegMicrophone: (outputPath, device) => ipcRenderer.invoke('start-ffmpeg-microphone', { outputPath, device }),
   stopFFmpegRecording: () => ipcRenderer.invoke('stop-ffmpeg-recording'),
   mergeAudioFiles: (microphonePath, systemAudioPath, outputPath) => ipcRenderer.invoke('merge-audio-files', { microphonePath, systemAudioPath, outputPath }),
   getPulseAudioSources: () => ipcRenderer.invoke('get-pulseaudio-sources'),
